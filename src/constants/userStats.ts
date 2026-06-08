e6aa85b8ba14f8cff824c24185cd89602c784c67
Peace Nnorom <nnorompeace14@gmail.com>
2026-06-08 21:03:33 +0100

    feat: add app constants

---
+++ src/constants/userStats.ts
@@ -0,0 +1,26 @@
+export const userStats = [
+  {
+    label: 'Users',
+    value: '2,453',
+    iconSrc: '/images/icon-stat-users.png',
+    iconBg: 'rgba(223, 24, 255, 0.1)',
+  },
+  {
+    label: 'Active Users',
+    value: '2,453',
+    iconSrc: '/images/icon-stat-active-users.png',
+    iconBg: 'rgba(87, 24, 255, 0.1)',
+  },
+  {
+    label: 'Users with Loans',
+    value: '12,453',
+    iconSrc: '/images/icon-stat-loans.png',
+    iconBg: 'rgba(245, 95, 68, 0.1)',
+  },
+  {
+    label: 'Users with Savings',
+    value: '102,453',
+    iconSrc: '/images/icon-stat-savings.png',
+    iconBg: 'rgba(255, 51, 102, 0.1)',
+  },
+]
